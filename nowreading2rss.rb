363a21e6c663ac13fc09bfff70482675ce7c3afd
Logan Capaldo <logancapaldo@gmail.com>
2012-12-15 14:05:19 +0000

    All the basic functionality
    Takes a user's now reading list and spits out some RSS for it. "Remembers" what
    you've already read.

--- nowreading2rss.rb
+++ nowreading2rss.rb
@@ -6,6 +6,28 @@ TITLE = "Example Title"
 LINK = "http://www.example.com/Link"
 DESCRIPTION = "Example Description"
 USERNAME = "ExampleUserName"
+OUTPUTFILE = "example.xml"
+HISTORICALINFO = "example.json"
+
+bookinfo = {}
+
+if File.exist? HISTORICALINFO
+  bookinfo = JSON.parse(File.read(HISTORICALINFO))
+end
+
+shelf = Shelfari.new
+
+now_reading = JSON.parse(shelf.now_reading(USERNAME))
+now_reading["books"].each do |raw_book|
+  book_id = raw_book["bookId"]
+  unless bookinfo.has_key? book_id.to_s
+    bookinfo[book_id.to_s] = Time.now.to_i
+  end
+end
+
+File.open(HISTORICALINFO, "w") { |f| f.puts  JSON.generate(bookinfo) }
+  
+
 
 
 content = RSS::Maker.make("2.0") do |m|
@@ -14,18 +36,14 @@ content = RSS::Maker.make("2.0") do |m|
   m.channel.description = DESCRIPTION
   m.items.do_sort = true
   
-  shelf = Shelfari.new
-  now_reading = JSON.parse(shelf.now_reading(USERNAME))
-  now_reading["books"].each do |raw_book|
-    book = JSON.parse(shelf.book(raw_book["bookId"]))[0]
+  bookinfo.each do |k, v|
+    book = JSON.parse(shelf.book(k.to_i))[0]
     i = m.items.new_item
     i.title = book["title"]
     i.link = book["url"]
-    i.date = Time.now
+    i.date = Time.at(v)
     i.description = "I am now reading #{book["title"]} by #{book["author"]}"
   end
 end
 
-puts content 
-    
-
+File.open(OUTPUTFILE, "w") { |f| f.puts content }
